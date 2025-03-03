cd353a73af9a521117c351ff033e982709bcb6e4
seiis <seiisland@gmail.com>
2025-03-03 13:00:59 +0900

    退職届年月日フォーマット適用

--- resources/views/confirmation.blade.php
+++ resources/views/confirmation.blade.php
@@ -73,7 +73,8 @@
             <br>
 
             <p>前略</p>
-            <p>私、{{ session('form.name') }}は一身上の都合により、{{ session('form.desired_resignation_date', '退職日未入力') }}をもちまして退職いたしたく、ここに届出いたします｡
+            <p>私、{{ session('form.name') }}は一身上の都合により、{{ \Carbon\Carbon::parse(session('form.desired_resignation_date', '退職日未入力'))->format('Y年m月d日') }}をもちまして退職いたしたく、ここに届出いたします｡
+            </p>
             </p>
             <p>なお､「離職票」及び「給与所得者の源泉徴収票」並びに「社会保険資格喪失証明書」のご依頼をいたしますので､上記住所宛てにお手配のほどよろしくお願いいたします｡</p>
             <p>併せて､給与のお振込先は以下の通りですのでお振込の程よろしくお願い申し上げます｡</p>
@@ -85,7 +86,7 @@
             <p class="text-center font-weight-bold">{{ session('form.bank_name', '銀行名未入力') }}</p>
             <p class="text-center font-weight-bold">{{ session('form.account_type', '口座種別未入力') }}</p>
             <p class="text-center font-weight-bold">{{ session('form.account_number', '口座番号未入力') }}</p>
-            
+
             <p class="text-right font-weight-bold">以上</p>
         </div>
 
